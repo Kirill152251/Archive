ceb390a1edb9aa450334f413cd5fa89202d29117
Lliril <kirya.ermalenok@mail.ru>
2022-04-25 12:03:34 +0300

    start inlement bottom nav bar

---
+++ app/src/main/java/com/example/archive/ui/items/ItemsScreen.kt
@@ -0,0 +1,10 @@
+package com.example.archive.ui.items
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination
+fun ItemsScreen() {
+
+}
---
+++ app/src/main/java/com/example/archive/ui/settings/SettingsScreen.kt
@@ -0,0 +1,10 @@
+package com.example.archive.ui.settings
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination
+fun SettingsScreen() {
+
+}
