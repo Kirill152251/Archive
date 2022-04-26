fb397f949d9e53f80da5c0061593521ab00d6ab6
Lliril <kirya.ermalenok@mail.ru>
2022-04-26 10:35:21 +0300

    create string resources

--- app/src/main/java/com/example/archive/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/archive/ui/profile/ProfileScreen.kt
@@ -7,9 +7,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -17,19 +14,15 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
 import com.example.archive.R
 import com.example.archive.ui.theme.DeepBlue
 import com.example.archive.viewmodels.profile.ProfileScreenEvent
-import com.example.archive.viewmodels.profile.ProfileScreenState
 import com.example.archive.viewmodels.profile.ProfileViewModel
 import com.example.archive.viewmodels.profile.TypeOfInfo
 import com.ramcosta.composedestinations.annotation.Destination
-import kotlinx.coroutines.launch
 
 @Composable
 @Destination
@@ -56,11 +49,11 @@ fun ProfileScreen(
         )
         Spacer(modifier = Modifier.height(12.dp))
         Text(
-            text = "Change profile picture",
+            text = stringResource(id = R.string.change_profile_picture),
             color = MaterialTheme.colors.onBackground,
             fontSize = 16.sp,
             modifier = Modifier.clickable {
-                Toast.makeText(context, "Click", Toast.LENGTH_SHORT).show()
+                
             }
         )
         Spacer(modifier = Modifier.height(24.dp))
@@ -124,7 +117,7 @@ fun DisplayTopBar(context: Context, viewModel: ProfileViewModel, toast: String) 
                 }
             ) {
                 Text(
-                    text = "Save",
+                    text = stringResource(id = R.string.save),
                     color = MaterialTheme.colors.onBackground,
                     fontSize = 18.sp
                 )
